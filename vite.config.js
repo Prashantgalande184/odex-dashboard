44b7cff421bd27bb726a4306580d0cfb74d4f122
Prashantgalande184 <prashantgalande184j@gmail.com>
2026-03-21 17:55:04 +0530

    Fix: Change base path to /

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/odex-dashboard/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
-    open: true
+    open: true,
   },
   build: {
     outDir: 'docs',
     minify: 'esbuild',
-    sourcemap: false
-  }
+    sourcemap: false,
+  },
 })
