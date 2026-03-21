18beb1dadbbe0eb9d7c9bb66e5ebbe971536d396
Prashantgalande184 <109078817+Prashantgalande184@users.noreply.github.com>
2026-03-21 19:28:07 +0530

    Add monitoring configuration URLs

---
+++ src/utils/monitoringConfig.js
@@ -0,0 +1,9 @@
+// Monitoring URLs configuration
+
+const monitoringConfig = {
+    serviceA: "https://monitoring.serviceA.com",
+    serviceB: "https://monitoring.serviceB.com",
+    serviceC: "https://monitoring.serviceC.com"
+};
+
+module.exports = monitoringConfig;
